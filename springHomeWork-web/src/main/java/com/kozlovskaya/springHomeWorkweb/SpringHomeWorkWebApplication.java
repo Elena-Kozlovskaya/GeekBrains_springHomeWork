05d3935e561218d8516b3d378189919687c51dae
Elena Kozlovskaya <lenadk23@gmail.com>
2021-11-18 16:49:05 +0500

    Initial commit

---
+++ springHomeWork-web/src/main/java/com/kozlovskaya/springHomeWorkweb/SpringHomeWorkWebApplication.java
@@ -0,0 +1,13 @@
+package com.kozlovskaya.springHomeWorkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeWorkWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHomeWorkWebApplication.class, args);
+	}
+
+}
